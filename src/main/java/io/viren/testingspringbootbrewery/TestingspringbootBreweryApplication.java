7cb730b837c74ff965ff3a4430395e96334ace2a
Viren <virsharm2@publicisgroupe.net>
2020-04-13 13:14:49 +0530

    Initial commit

---
+++ src/main/java/io/viren/testingspringbootbrewery/TestingspringbootBreweryApplication.java
@@ -0,0 +1,13 @@
+package io.viren.testingspringbootbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingspringbootBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingspringbootBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/viren/testingspringbootbrewery/TestingspringbootBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.viren.testingspringbootbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingspringbootBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
